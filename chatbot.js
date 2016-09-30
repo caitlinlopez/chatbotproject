44cccd74d67c8cc640b6c20f53e0e26a9e1f6732
caitlin <caitlinlopez@users.noreply.github.com>
2016-09-30 11:02:57 -0700

    You can ask the chatbot a question using the "enter" key instead of clicking the button.

--- chatbot.js
+++ chatbot.js
@@ -3,15 +3,38 @@ var message = {
   "x" : "y"
 }
 
-function button(){
-  var userInput = $("#input").val();
-  $("#chat-area").append(userInput + "<br>");
-
-  var answer = message[userInput];
-  $("#chat-area").append("person" + ": " + answer + "<br>" + "<br>");
-  //document.getElementById("chat-area").innerHTML = userInput + ": " + answer;
+function enter(event){
+var x = event.keyCode;
+if(x == 13){
+  button();
+}
 
 }
+
+function button(){
+  var userInput = $("#input").val();
+  var answer = message[userInput];
+
+  if (answer == undefined){
+    var fallback = ["what", "???"];
+    //var rand = Math.floor((Math.random() * fallback.length));
+
+    $("#chat-area").append(userInput + "<br>");
+
+    $("#chat-area").append("person" + ": " + fallback + "<br>" + "<br>");
+  }
+
+  else {
+  $("#chat-area").append(userInput + "<br>");
+
+  $("#chat-area").append("person" + ": " + answer + "<br>" + "<br>");
+  //document.getElementById("chat-area").innerHTML = userInput + ": " + answer;
+}
+
+
+}
+
+
 
 /*
   var x = {
@@ -21,4 +44,14 @@ function button(){
   var r = x["z" or userInput]; --> c
 
   var userInput = "hello";
+
+  if (answer == undefined){
+    $("#chat-area").append(userInput + "<br>");
+
+    var answerr = "what";
+    $("#chat-area").append("person" + ": " + answer + "<br>" + "<br>");
+  }
+
+  input = input.toLowerCase()
+
 */
