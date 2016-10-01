f73cba36cac9f1152565c2f6c4c2e7c2db00f4ea
caitlin <caitlinlopez@users.noreply.github.com>
2016-10-01 14:56:17 -0700

    The chatbot is not case-sensitive.

--- chatbot.js
+++ chatbot.js
@@ -1,5 +1,5 @@
 var message = {
-  "hello" : "hi",
+  "what's your name?" : "Alexander Hamilton. My name is Alexander Hamilton, and there's a million things I haven't done, but just you wait, just you wait.",
   "x" : "y"
 }
 
@@ -13,21 +13,24 @@ if(x == 13){
 
 function button(){
   var userInput = $("#input").val();
-  var answer = message[userInput];
+
+  var lowerCase = userInput.toLowerCase();
+
+  var answer = message[lowerCase];
 
   if (answer == undefined){
-    var fallback = ["what", "???"];
+    var fallback = ["My dog speaks more eloquently than thee!", "???"];
     var rand = fallback[Math.floor((Math.random() * fallback.length))];
 
-    $("#chat-area").append(userInput + "<br>");
+    $("#chat-area").append("You" + ": " + userInput + "<br>");
 
-    $("#chat-area").append("person" + ": " + rand + "<br>" + "<br>");
+    $("#chat-area").append("Hamilton" + ": " + rand + "<br>" + "<br>");
   }
 
   else {
-  $("#chat-area").append(userInput + "<br>");
+  $("#chat-area").append("You" + ": " + userInput + "<br>");
 
-  $("#chat-area").append("person" + ": " + answer + "<br>" + "<br>");
+  $("#chat-area").append("Hamilton" + ": " + answer + "<br>" + "<br>");
   //document.getElementById("chat-area").innerHTML = userInput + ": " + answer;
 }
 
