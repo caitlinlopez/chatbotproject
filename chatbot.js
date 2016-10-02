06cd237dc66a8a0f561736d5741f74dac8426290
caitlin <caitlinlopez@users.noreply.github.com>
2016-10-02 14:35:25 -0700

    If you ask the chatbot what time it is (using the question format "what time is it?"), it will tell you the current time.

--- chatbot.js
+++ chatbot.js
@@ -1,40 +1,54 @@
 var message = {
-  "what's your name?" : "Alexander Hamilton. My name is Alexander Hamilton, and there's a million things I haven't done, but just you wait, just you wait.",
-  "x" : "y"
-}
-
-function enter(event){
-var x = event.keyCode;
-if(x == 13){
-  button();
-}
-
+  "what's your name?" : ["Alexander Hamilton. My name is Alexander Hamilton, and there's a million things I haven't done, but just you wait, just you wait."],
+  "what time is it?": ["It is " + returnTime() + "."],
+  "take a break." : ["I am on my way."],
+  "can i buy you drink?" : ["That would be nice."],
+  "hello" : ["a", "b", "c"]
 }
 
 function button(){
   var userInput = $("#input").val();
-
   var lowerCase = userInput.toLowerCase();
-
   var answer = message[lowerCase];
 
   if (answer == undefined){
-    var fallback = ["My dog speaks more eloquently than thee!", "???"];
+    var fallback = ["My dog speaks more eloquently than thee!", "What?", ];
     var rand = fallback[Math.floor((Math.random() * fallback.length))];
 
     $("#chat-area").append("You" + ": " + userInput + "<br>");
 
     $("#chat-area").append("Hamilton" + ": " + rand + "<br>" + "<br>");
   }
-
+else if (answer == " ") {
+    $("#chat-area").append("You" + ": " + "suck" + "<br>");
+}
   else {
-  $("#chat-area").append("You" + ": " + userInput + "<br>");
+  var rAnswer = answer[Math.floor((Math.random() * answer.length))];
 
-  $("#chat-area").append("Hamilton" + ": " + answer + "<br>" + "<br>");
-  //document.getElementById("chat-area").innerHTML = userInput + ": " + answer;
+  $("#chat-area").append("You" + ": " + userInput + "<br>");
+  $("#chat-area").append("Hamilton" + ": " + rAnswer + "<br>" + "<br>");
+  }
 }
 
+function returnTime(){
+  var now = new Date(Date.now());
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
 
+  if (hours <= 11){
+    return hours + ":" + minutes + " AM";
+  }
+  else{
+    return hours + ":" + minutes + " PM";
+  }
+}
+
+function enter(event){
+  var x = event.keyCode;
+
+  if(x == 13){
+    button();
+  }
 }
 
 
